148541339c955a24cd83d14e3ecfbf84842845fd
rfg9yy@virginia.edu <rfg9yy@virginia.edu>
2020-03-29 09:32:53 -0400

    added custom button to ui controller

--- App/ViewController.swift
+++ App/ViewController.swift
@@ -9,11 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var bottomCustomButton = CustomButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupBottomButtonConstraints()
+        addActionToBottomButton()
     }
+    
+    func setupBottomButtonConstraints() {
+        view.addSubview(bottomCustomButton)
+        bottomCustomButton.translatesAutoresizingMaskIntoConstraints = false
+        bottomCustomButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        bottomCustomButton.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        bottomCustomButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        bottomCustomButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -200).isActive = true
+    }
+    
+    func addActionToBottomButton() {
+        bottomCustomButton.addTarget(self, action: #selector(bottomButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc func bottomButtonTapped() {
+        bottomCustomButton.shake()
+    }
+    
 
 
 }
