8d9cb09df55ac0a8e31e070ef9acb0aed1eb3272
Wei Wang <weiwang701257@gmail.com>
2020-03-29 10:10:43 -0700

    completed fifth page survey controller

--- App/FifthPageSurveyController.swift
+++ App/FifthPageSurveyController.swift
@@ -9,9 +9,38 @@
 import Foundation
 import UIKit
 
-class FifthPageSurveyController: UIViewController {
+class FifthPageSurveyController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 3
+    }
+    
+    
+    @IBOutlet weak var pickerList: UIPickerView!
+    
+    var itemList: [String] = [String]()
+    var userChoice: String = "0 - 1 Day"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.pickerList.delegate = self
+        self.pickerList.dataSource = self
+        
+        itemList = ["0 - 1 Day", "2 - 4 Days", "5+ Days"]
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return itemList[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        userChoice = itemList[row]
     }
 }
