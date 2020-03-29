23ca1ef6605d40e04a9ce9c3636d16475abd4e49
rfg9yy@virginia.edu <rfg9yy@virginia.edu>
2020-03-29 09:56:15 -0400

    added embed video too uncontrolled. don't know how to put it in tho

--- App/ViewController.swift
+++ App/ViewController.swift
@@ -10,12 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var myWebView: UIWebView!
+    
     var bottomCustomButton = CustomButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupBottomButtonConstraints()
         addActionToBottomButton()
+        getVideo(videoCode: "c2yrVRtkir0")
     }
     
     func setupBottomButtonConstraints() {
@@ -35,6 +38,11 @@ class ViewController: UIViewController {
         bottomCustomButton.shake()
     }
     
+    func getVideo(videoCode:String) {
+        let url = URL(string: "https://www.youtube.com/embed/\(videoCode)")
+        myWebView.loadRequest(URLRequest(url: url!))
+        
+    }
 
 
 }
