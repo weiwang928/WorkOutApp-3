699fbc82ee2eff3ff920e61bc0c77398d7f093ce
Wei Wang <weiwang701257@gmail.com>
2020-03-28 19:48:26 -0700

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by user921391 on 3/28/20.
+//  Copyright © 2020 Wei Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
